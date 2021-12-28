f21b52a5959cae0ed4b1c580b37ed5eaaa312bf5
hyoyoung-OH <lisa4397@gmail.com>
2021-12-28 13:02:28 +0900

    add directory MSA

---
+++ MSA/java1/src/Variable.java
@@ -0,0 +1,19 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		//변수: 변할 수 있는 값
+		
+		int a = 1; //integer 정수
+		System.out.println(a);
+		
+		double b = 1.1; // double 실수 ... -2.0 -1.0 0 1.0 2.0 ...
+		System.out.println(b);
+		
+		String c = "hihihi"; // String 문자열
+		System.out.println(c);
+		
+		
+	}
+
+}
