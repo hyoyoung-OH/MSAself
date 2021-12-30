e75814ed11c62786cfe09a37b33f88094566a2f9
hyoyoung-OH <lisa4397@gmail.com>
2021-12-30 18:28:24 +0900

    boolean study

---
+++ MSA/java-flow-control/src/BooleanApp.java
@@ -0,0 +1,17 @@
+
+public class BooleanApp {
+ public static void main(String[] args) {
+	 System.out.println("one");
+	 System.out.println(1);
+	 
+	 System.out.println(true);
+	 System.out.println(false);
+	 
+	 
+	 String foo = "Hello world";
+	 System.out.println(foo.contains("world"));
+	 System.out.println(foo.contains("love"));
+	 
+	 //contains() 함수는 대상무나자열에 특정문자열이 포함되어 있는 지 확인하는 함수
+ }
+}
